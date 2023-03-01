ba29a12b7704036494bfddb52bcc58ae3355dd83
Neobtt <brenotqgelias@gmail.com>
2023-03-01 20:11:49 -0300

    direct eclipse/java repository test
    Change-Id: I401dad518ce4e8a4937fc0f76015c378cdbbd499

---
+++ src/dio/firstcode.java
@@ -0,0 +1,18 @@
+package dio;
+
+import dio.model.Cat;
+
+public class firstcode {
+
+	public static void main(String[] args) {
+		
+		Cat cat = new Cat();
+		
+		System.out.println(cat);
+		/*int a = 10;
+		int b = 11;
+		System.out.println("Hello World! " + (a+b));
+	*/
+
+	}
+}
