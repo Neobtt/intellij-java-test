c5d66ea4044c63a0e00549591bb65c9b72aa3752
Neobtt <brenotqgelias@gmail.com>
2023-03-17 04:28:53 -0300

    First IJ commit

---
+++ Intellij/Intellij-test/src/br/com/dio/FirstProgram.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+/*        int a = 10;
+        int b = 11;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
